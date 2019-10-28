f6327b35cd1c9a35105d2104ab63871b9d94024a
Pete D <peted70@hotmail.com>
2019-10-28 19:48:27 +0000

    HoloLens Connection
    Able to connect render host to Client app running on HoloLens

--- ModelViewer/ViewModels/ConnectPageViewModel.cpp
+++ ModelViewer/ViewModels/ConnectPageViewModel.cpp
@@ -5,6 +5,7 @@
 #include <pplawait.h>
 #include <chrono>
 #include <HolographicAppRemoting\Streamer.h>
+#include <Common\HolographicDeviceResources.h>
 
 using namespace ViewModels;
 using namespace std::chrono;
@@ -28,27 +29,105 @@ void ConnectPageViewModel::OnRemoteConnected(RemoteContext context)
 
 }
 
+String^ PlatformFailureToString(ConnectionFailureReason reason)
+{
+	switch (reason)
+	{
+	case ConnectionFailureReason::AuthenticationFailed:
+		return L"Authentication Failed";
+	case ConnectionFailureReason::Canceled:
+		return L"Canceled";
+	case ConnectionFailureReason::ConnectionLost:
+		return L"Connection Lost";
+	case ConnectionFailureReason::DeviceLost:
+		return L"Device Lost";
+	case ConnectionFailureReason::DisconnectRequest:
+		return L"Disconnect Request";
+	case ConnectionFailureReason::HandshakeConnectionFailed:
+		return L"Handshake Connection Failed";
+	case ConnectionFailureReason::HandshakeFailed:
+		return L"Handshake Failed";
+	case ConnectionFailureReason::HandshakePortBusy:
+		return L"Handshake Port Busy";
+	case ConnectionFailureReason::HandshakeUnreachable:
+		return L"Handshake Unreachable";
+	case ConnectionFailureReason::IncompatibleTransportProtocols:
+		return L"Incompatible Transport Protocols";
+	case ConnectionFailureReason::NoServerCertificate:
+		return L"No Server Certificate";
+	case ConnectionFailureReason::ProtocolError:
+		return L"Protocol Error";
+	case ConnectionFailureReason::ProtocolVersionMismatch:
+		return L"Protocol Version Mismatch";
+	case ConnectionFailureReason::RemotingVersionMismatch:
+		return L"Remoting Version Mismatch";
+	case ConnectionFailureReason::TransportConnectionFailed:
+		return L"Transport Connection Failed";
+	case ConnectionFailureReason::TransportPortBusy:
+		return L"Transport Port Busy";
+	case ConnectionFailureReason::TransportUnreachable:
+		return L"Transport Unreachable";
+	default:
+		return L"Unknown Failure";
+	}
+}
+
+#include <future>
+
+future<void> ConnectPageViewModel::SetStatusText(String^ text)
+{
+	auto disp = ::Windows::ApplicationModel::Core::CoreApplication::MainView->CoreWindow->Dispatcher;
+	co_await disp->RunAsync(::Windows::UI::Core::CoreDispatcherPriority::Normal,
+		ref new ::Windows::UI::Core::DispatchedHandler([text, this]() 
+			{
+				if (StatusText->Length() > 0) StatusText += ref new String(L"\n");
+				StatusText += text; 
+			}));
+}
+
 future<void> ConnectPageViewModel::Connect()
 {
-	Utility::Out(L"At Start of Connect");
+	if (Loading == true)
+		return;
 
-	co_await 5s;
+	Utility::Out(L"At Start of Connect");
+	StatusText = L"";
 
 	// RAII-style for ensuring that the progress gets cleared robustly
 	auto loader = make_unique<LoadingWrapper>([this]() { Loading = true; }, [this]() { Loading = false; });
+
+	co_await 5s;
 
 	// Create on a thread pool thread?
 	try
 	{
 		CreateRemoteContext(_remoteContext, 20000, false, PreferredVideoCodec::Default);
 		_holographicSpace = HolographicSpace::CreateForCoreWindow(nullptr);
-		//_holographicSpace->SetDirect3D11Device();
-		
+
+		_deviceResources = std::make_shared<DXHelper::HolographicDeviceResources>();
+		_deviceResources->SetHolographicSpace(_holographicSpace);
+
+		// TODO: need to wire up the device lost and device restored events here...
+
 		winrt::weak_ref<winrt::Microsoft::Holographic::AppRemoting::IRemoteContext> remoteContextWeakRef = _remoteContext;
 
 		_onConnectedEventRevoker = _remoteContext.OnConnected(winrt::auto_revoke, [this, remoteContextWeakRef]() {
 				if (auto remoteContext = remoteContextWeakRef.get())
 				{
+					auto state = remoteContext.ConnectionState();
+					if (state == ConnectionState::Connected)
+					{
+						SetStatusText("Connected");
+					}
+					else if (state == ConnectionState::Connecting)
+					{
+						SetStatusText("Connecting");
+					}
+					else if (state == ConnectionState::Disconnected)
+					{
+						SetStatusText("Disconnected");
+					}
+
 					// Update UI state
 					//this->OnRemoteConnected(remoteContext.as<RemoteContext>());
 				}
@@ -58,6 +137,7 @@ future<void> ConnectPageViewModel::Connect()
 				if (auto remoteContext = remoteContextWeakRef.get())
 				{
 					Utility::Out(L"Disconnected with reason %d", failureReason);
+					SetStatusText(PlatformFailureToString(failureReason));
 
 					// Update UI
 
@@ -67,13 +147,16 @@ future<void> ConnectPageViewModel::Connect()
 						failureReason == ConnectionFailureReason::ConnectionLost)
 					{
 						Utility::Out(L"Reconnecting...");
+						SetStatusText(L"Reconnecting...");
 
-						//ConnectOrListen();
+						_remoteContext.Disconnect();
+						_remoteContext.Connect(_ipAddress->Data(), 8265);
 					}
 					// Failure reason None indicates a normal disconnect.
 					else if (failureReason != ConnectionFailureReason::None)
 					{
 						Utility::Out(L"Disconnected with unrecoverable error, not attempting to reconnect.");
+						SetStatusText(L"Disconnected with unrecoverable error, not attempting to reconnect.");
 					}
 				}
 			});
@@ -90,6 +173,7 @@ future<void> ConnectPageViewModel::Connect()
 	catch (winrt::hresult_error & e)
 	{
 		Utility::Out(L"Connect failed with hr = 0x%08X", e.code());
+		SetStatusText(L"Connect failed");
 	}
 
 	Utility::Out(L"Connected");
@@ -107,6 +191,19 @@ void ConnectPageViewModel::Loading::set(bool val)
 	if (_loading == val)
 		return;
 	_loading = val;
+	OnPropertyChanged(getCallerName(__FUNCTION__));
+}
+
+String^ ConnectPageViewModel::StatusText::get()
+{
+	return _statusText;
+}
+
+void ConnectPageViewModel::StatusText::set(String^ val)
+{
+	if (_statusText == val)
+		return;
+	_statusText = val;
 	OnPropertyChanged(getCallerName(__FUNCTION__));
 }
 
--- ModelViewer/ViewModels/ConnectPageViewModel.h
+++ ModelViewer/ViewModels/ConnectPageViewModel.h
@@ -3,6 +3,7 @@
 #include "Common/ViewModelBase.h"
 #include <winrt/Microsoft.Holographic.AppRemoting.h>
 #include <winrt/Windows.Graphics.Holographic.h>
+#include "Common/HolographicDeviceResources.h"
 
 namespace ViewModels
 {
@@ -13,6 +14,8 @@ namespace ViewModels
 		ConnectPageViewModel();
 
 		property bool Loading { bool get(); void set(bool val); }
+		property String^ StatusText { String^ get(); void set(String^ val); }
+
 		property Windows::UI::Xaml::Input::ICommand^ ConnectCommand;
 		property Platform::String^ IPAddress { Platform::String^ get(); void set(Platform::String^ val); }
 
@@ -21,13 +24,16 @@ namespace ViewModels
 		std::future<void> Connect();
 
 		void ConnectPageViewModel::OnRemoteConnected(winrt::Microsoft::Holographic::AppRemoting::RemoteContext context);
+		std::future<void> SetStatusText(String^ text);
 
 		bool _loading = false;
+		Platform::String^ _statusText;
 		Platform::String^ _ipAddress = L"127.0.0.1";
 
 		// RemoteContext used to connect with a Holographic Remoting player and display rendered frames
 		winrt::Microsoft::Holographic::AppRemoting::RemoteContext _remoteContext = nullptr;
 		winrt::Windows::Graphics::Holographic::HolographicSpace _holographicSpace = nullptr;
+		std::shared_ptr<DXHelper::HolographicDeviceResources> _deviceResources;
 
 		winrt::Microsoft::Holographic::AppRemoting::IRemoteContext::OnConnected_revoker _onConnectedEventRevoker;
 		winrt::Microsoft::Holographic::AppRemoting::IRemoteContext::OnDisconnected_revoker _onDisconnectedEventRevoker;
